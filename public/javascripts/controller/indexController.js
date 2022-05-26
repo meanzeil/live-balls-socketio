165443c39a214a1269dfc6929d6c8513f4648095
meanzeil <melih.aydnltn1@gmail.com>
2022-05-26 22:28:31 +0300

    newUser add

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,15 +1,27 @@
 app.controller('indexController', ['$scope','indexFactory',($scope, indexFactory)=>{
 
-    const connectionOptions = {
-        reconnectAttempt: 3,
-        reconnectionDelay: 600
+    $scope.init = () =>{
+        const username = prompt('Lütfen Kullanıcı Adınızı Girin');
+
+        if (username)
+            initSocket(username)
+        else
+            return false;
     };
 
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-        .then((socket)=>{
-            console.log('bağlantı gerçekleşti', socket);
-        }).catch((err)=>{
-            console.log(err);
-        });
+    function initSocket(username) {
+
+        const connectionOptions = {
+            reconnectAttempt: 3,
+            reconnectionDelay: 600
+        };
+
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+            .then((socket)=>{
+                socket.emit('newUser', { username: username });
+            }).catch((err)=>{
+                console.log(err);
+            });
+    }
 
 }]);
