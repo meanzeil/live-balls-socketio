f3c96d3d2188f52fae51c1ca9bc671c9f565c903
meanzeil <melih.aydnltn1@gmail.com>
2022-05-30 09:46:31 +0300

    heroku update v2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Live Balls' });
+  res.render('index', { title: 'meanzeil Live Balls' });
 });
 
 router.get('/getEnv', function(req, res, next) {
